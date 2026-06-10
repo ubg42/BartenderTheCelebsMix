371a533afbbede7c2c93ebb0df4ebf7793886567
ubg42 <redtomatotiger@gmail.com>
2026-06-10 17:55:05 +1000

    Add files via upload

---
+++ prevent-keys.js
@@ -0,0 +1,1 @@
+!function(){if(window.self!==window.top&&!/\/unity(_webgl)?\//.test(window.location.href)){var o={Space:!0,ArrowUp:!0,ArrowDown:!0,ArrowLeft:!0,ArrowRight:!0};window.addEventListener("keydown",(function(n){o[n.code]&&n.preventDefault()}),!1)}}();
